8834b3c8ef82a6347366369122d3687b534025b9
Patrick Horn <patrick.horn@gmail.com>
2012-04-27 02:16:36 -0700

    Add Uint8ArrayStream -- not fully supported

--- protobuf.js
+++ protobuf.js
@@ -648,6 +648,47 @@ PROTO.ByteArrayStream.prototype.valid = function() {
 PROTO.ByteArrayStream.prototype.getArray = function() {
     return this.array_;
 };
+
+PROTO.Uint8ArrayStream = function(arr) {
+    this.array_ = arr || new Uint8Array(4096);
+    this.read_pos_ = 0;
+    this.write_pos_ = 0;
+}
+PROTO.Uint8ArrayStream.prototype._realloc = function(new_size) {
+    this.array_ = new Uint8Array(Math.max(new_size, this.array_.length)
+				 + this.array_.length);
+}
+PROTO.Uint8ArrayStream.prototype.read = function(amt) {
+    if (this.read_pos_+amt > this.array_.length) {
+        return null;
+    }
+    var ret = this.array_.subarray(this.read_pos_, this.read_pos_+amt);
+    this.read_pos_ += amt;
+    return ret;
+};
+PROTO.Uint8ArrayStream.prototype.write = function(arr) {
+    if (this.write_pos_ + arr.length > this.array_.length) {
+	this.realloc_(this.write_pos_ + arr.length);
+    }
+    this.array_.set(arr, this.write_pos_);
+    this.write_pos_ += arr.length;
+};
+PROTO.Uint8ArrayStream.prototype.readByte = function() {
+    return this.array_[this.read_pos_ ++];
+};
+PROTO.Uint8ArrayStream.prototype.writeByte = function(byt) {
+    if (this.write_pos_ >= this.array_.length) {
+	this.realloc_(this.write_pos_ + 1);
+    }
+    this.array_[this.write_pos_++] = byt;
+};
+PROTO.Uint8ArrayStream.prototype.valid = function() {
+    return this.read_pos_ < this.array_.length;
+};
+PROTO.Uint8ArrayStream.prototype.getArray = function() {
+    return this.array_.subarray(0, this.write_pos_);
+};
+
 /**
  * @constructor
  * @param {string=} b64string  String to read from, or append to.
